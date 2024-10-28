e6059074c338555299f18a81eaf0297aec98298f
James Cahill <jcahill773@gmail.com>
2024-10-28 10:17:53 +0000

    (lectures/5/slide_extras): Add missing sql query/response pair

---
+++ lectures/5_sql_dml/slide_extras/1_1_custom.sql
@@ -0,0 +1,29 @@
+/*
+DISTINCT Statement
+    -> Usage
+        -> Why do we need the the DISTINCT statement?
+            -> Query 1
+*/
+
+SELECT p_town FROM mccPlayer;
+
+/*
++------------+
+| p_town     |
++------------+
+| Morpeth    |
+| Morpeth    |
+| Alnwick    |
+| Alnwick    |
+| Morpeth    |
+| Byker      |
+| Morpeth    |
+| Byker      |
+| Durham     |
+| Sunderland |
+| Durham     |
+| Sunderland |
++------------+
+12 rows in set (0.00 sec)
+*/
+
